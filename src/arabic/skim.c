8f065a9c3b5cd1681b558589ee442b64284051a3
MJaoune <mjaoune55@gmail.com>
2021-01-27 19:42:31 +0200

    Hotfix (Ignoring harakat)

--- src/arabic/skim.c
+++ src/arabic/skim.c
@@ -140,7 +140,7 @@ size_t Arabic_SkimString(const uint32_t *str, uint32_t **out)
 		}
 
 		//Other letters not looked in this function (Excluding harakat)
-		if(!(str[i] >= 0xD890 && str[i] <= 0xD89A) || (str[i] >= 0xD98B && str[i] <= 0xD99F))
+		if(!((str[i] >= 0xD890 && str[i] <= 0xD89A) || (str[i] >= 0xD98B && str[i] <= 0xD99F)))
 			InsertChar(str[i], out, &strLengthNew, strLength);
 	}
 
